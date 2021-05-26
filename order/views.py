c0b5a1618808c5a64b3d57e52a6a70b84cafbed7
hhdMrLion <55070348+hhdMrLion@users.noreply.github.com>
2021-05-26 10:59:31 +0800

    '开发订单支付和取消接口'

--- order/views.py
+++ order/views.py
@@ -1,12 +1,15 @@
 import json
 
 from django import http
+from django.db import transaction
+from django.db.models import F
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import FormView
 from django.views.generic.detail import BaseDetailView
 
 from order import serializers
+from order.choices import OrderStatus
 from order.forms import SubmitTicketOrderForm
 from order.models import Order
 from utils.response import BadRequestJsonResponse
@@ -52,6 +55,45 @@ class OrderDetail(BaseDetailView):
         return Order.objects.filter(user=user, is_valid=True)
 
     def get(self, request, *args, **kwargs):
+        """ GET：订单详情 """
         order_obj = self.get_object()
         data = serializers.OrderDetailSerializer(order_obj).to_dict()
         return http.JsonResponse(data)
+
+    def post(self, request, *args, **kwargs):
+        """ POST：订单支付 """
+        # 选择支付方式
+        # 数据验证
+        order_obj = self.get_object()
+        if order_obj.status == OrderStatus.SUBMIT:
+            # 调用真实的支付
+            # 改变订单状态
+            order_obj.status = OrderStatus.PAID
+            order_obj.save()
+            order_obj.order_items.update(status=OrderStatus.PAID)
+            return http.HttpResponse('', status=201)
+        return http.HttpResponse('', status=200)
+
+    def delete(self, request, *args, **kwargs):
+        """ DELETE：订单删除 """
+        pass
+
+    @transaction.atomic()
+    def put(self, request, *args, **kwargs):
+        """ PUT：订单取消 """
+        # 获取订单对象
+        order_obj = self.get_object()
+        # 数据验证，状态判断
+        if order_obj.status == OrderStatus.SUBMIT:
+            # 改变状态
+            order_obj.status = OrderStatus.CANCELED
+            order_obj.save()
+            items = order_obj.order_items.filter(status=OrderStatus.SUBMIT)
+            # 加回已经扣减的库存
+            for item in items:
+                obj = item.content_object
+                obj.remain_stock = F('remain_stock') + item.count
+                obj.save()
+            items.update(status=OrderStatus.CANCELED)
+            return http.HttpResponse('', status=201)
+        return http.HttpResponse('', 200)
